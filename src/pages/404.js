e44c58631b5108b503096269cb35777f01a10869
Shahrukh <shahrukh1494@gmail.com>
2024-07-30 13:05:35 +0530

    not found page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div className="not-found-page">
+        <div className="something-went-wrong">Something went wrong!</div> 
+        <div className="try-again-text">Please try again</div>
+      </div>
+    </>
+  );
+}
