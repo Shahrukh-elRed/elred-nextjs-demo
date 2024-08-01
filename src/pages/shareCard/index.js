a212773959c178e6b7c95818f7cb968d24068817
Shahrukh <shahrukh1494@gmail.com>
2024-08-01 17:16:39 +0530

    group card

--- src/pages/shareCard/index.js
+++ src/pages/shareCard/index.js
@@ -7,8 +7,8 @@ import {
 import Head from "next/head";
 import NotFound from "../component/notFound";
 
-function ShareCard({ data, userCode }) {
-  if (!userCode) {
+function ShareCard({ data, userCode, networkCode }) {
+  if (!userCode && !networkCode) {
     return <NotFound />;
   }
 
@@ -30,7 +30,11 @@ function ShareCard({ data, userCode }) {
       <div className="d-flex align-item-center justify-content-center height-100">
         <iframe
           allow="web-share"
-          src={`${webviewURL}share-card?userCode=${userCode}`}
+          src={
+            userCode
+              ? `${webviewURL}share-card?userCode=${userCode}`
+              : `${webviewURL}network-share-card?networkCode=${networkCode}`
+          }
           className="iframe-cont"
           title=""
         ></iframe>
@@ -42,15 +46,20 @@ function ShareCard({ data, userCode }) {
 export async function getServerSideProps({ res, query }) {
   res.setHeader("Cache-Control", "no-store");
   const userCode = query?.userCode ?? "";
+  const networkCode = query.networkCode ?? "";
 
   let url = `${baseURL}`;
   if (userCode) {
     url += `noSessionPreviewCardScreenshot?userCode=${userCode}`;
   }
 
+  if (networkCode) {
+    url += `webviewGetNetworkScreenshot?networkCode=${networkCode}`;
+  }
+
   const response = await fetch(url, {
     cache: "no-cache",
-    method: "POST",
+    method: userCode ? "POST" : "GET",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
@@ -62,7 +71,7 @@ export async function getServerSideProps({ res, query }) {
   const result = data?.result && data?.result?.length && data?.result[0];
 
   return {
-    props: { data: result, userCode },
+    props: { data: result, userCode, networkCode },
   };
 }
 
